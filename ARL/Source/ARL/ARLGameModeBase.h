26ec01f37e8bae72b696201396b82659a39122b5
pwj <1915986515@qq.com>
2023-09-14 23:28:12 +0800

    新建项目

---
+++ ARL/Source/ARL/ARLGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ARLGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARL_API AARLGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
